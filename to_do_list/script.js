1d309ebc50bfcab0d055a54fae5092142b76d49c
Priyanshu236 <75911276+Priyanshu236@users.noreply.github.com>
2021-08-13 01:17:11 +0530

    Add files via upload

---
+++ to_do_list/script.js
@@ -0,0 +1,53 @@
+const inputbox=document.querySelector('.inputField input');
+const addbtn=document.querySelector('.inputField button');
+const delbtn=document.querySelector('.clearall');
+
+const todolist=document.querySelector('.addtask');
+const pendtasks=document.querySelector('.num');
+addbtn.onclick=()=>{
+    
+     let userdata=inputbox.value;
+    let getlocalStorage=localStorage.getItem("New Todo");
+    if(getlocalStorage==null){
+        listArr=[];
+    }else{
+
+        listArr=JSON.parse(getlocalStorage);
+        
+    }
+    listArr.push(userdata);
+    localStorage.setItem("New Todo",JSON.stringify(listArr))
+    showtasks();
+}
+showtasks();
+
+function showtasks(){
+ let getstorage=localStorage.getItem("New Todo")
+ if(getstorage==null){
+     listArr=[]
+ }   else{
+     listArr=JSON.parse(getstorage)
+ }
+ let newLiTag='';
+ let size=0;
+ listArr.forEach((element,index) => {
+     size=size+1;
+    newLiTag+=`<li>${element} <span onclick="deleteTask(${index})";>Del</span></li>`;
+});
+pendtasks.innerHTML=size;
+inputbox.value="";
+todolist.innerHTML=newLiTag;
+}
+
+function deleteTask(index){
+    let getstorage=localStorage.getItem("New Todo");
+    listArr=JSON.parse(getstorage)
+    listArr.splice(index,1);
+    localStorage.setItem("New Todo",JSON.stringify(listArr));
+    showtasks();
+}
+delbtn.onclick=()=>{
+    listArr=[];
+    localStorage.setItem("New Todo",JSON.stringify(listArr));
+    showtasks();
+}
